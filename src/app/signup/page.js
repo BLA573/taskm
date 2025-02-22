02ee1dbed2e9a9fd8ec5519a9231b33e9f4aff61
fikertag <149786264+fikertag@users.noreply.github.com>
2025-02-22 20:15:55 +0300

    last one for now

--- src/app/signup/page.js
+++ src/app/signup/page.js
@@ -60,7 +60,7 @@ export default function LoginSignupForm() {
             <path d="M6 3h2v2H6zm2 16h3v2H8zm8-16h2v2h-2zm-3 16h3v2h-3zm7-8V9h-2V7h-2V5h-2v2h-4V5H8v2H6v2H4v2H2v8h2v-4h2v4h2v-3h8v3h2v-4h2v4h2v-8zm-10 1H8V9h2zm6 0h-2V9h2z"></path>
           </svg>
 
-          <h1 className=" text-white text-2xl mb-5 mt-1">Task App</h1>
+          <h1 className=" text-white text-2xl mb-5 mt-1">Task Up</h1>
           <input
             type="text"
             className=" bg-[#3a3939] py-2 rounded-lg px-3 text-sm w-60 text-white outline-none mb-4 shadow-xl"
